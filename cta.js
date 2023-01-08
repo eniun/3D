10f51151fbeedc33d4c755f00b197aeb478b348c
Eniun <34781420+eniun@users.noreply.github.com>
2023-01-08 21:49:24 +0100

    Update cta.js

--- cta.js
+++ cta.js
@@ -12,19 +12,23 @@ AFRAME.registerComponent('cta', {
 
     el.addEventListener('mousedown', function (evt) {
       el.setAttribute('material', 'color', '#EF2D5E');
+      el.setAttribute('material', 'transparent', 'false');
     });
 
     el.addEventListener('mouseup', function (evt) {
       el.setAttribute('material', 'color', self.isMouseEnter ? '#24CAFF' : initialColor);
+      el.setAttribute('material', 'transparent', 'false');
     });
 
     el.addEventListener('mouseenter', function () {
       el.setAttribute('material', 'color', '#24CAFF');
+      el.setAttribute('material', 'transparent', 'false');
       self.isMouseEnter = true;
     });
 
     el.addEventListener('mouseleave', function () {
       el.setAttribute('material', 'color', initialColor);
+      el.setAttribute('material', 'transparent', 'false');
       self.isMouseEnter = false;
     });
   }
