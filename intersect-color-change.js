c2ab7379bd2da1634e858f9a4222358a2f0a9ae9
Eniun <34781420+eniun@users.noreply.github.com>
2023-01-08 19:05:05 +0100

    Update intersect-color-change.js

--- intersect-color-change.js
+++ intersect-color-change.js
@@ -13,7 +13,6 @@ AFRAME.registerComponent('intersect-color-change', {
     el.addEventListener('mousedown', function (evt) {
       el.setAttribute('material', 'color', '#EF2D5E');
       el.setAttribute('material', 'opacity', '0.5');
-      el.setAttribute('material', 'transparent', 'true');
     });
 
     el.addEventListener('mouseup', function (evt) {
